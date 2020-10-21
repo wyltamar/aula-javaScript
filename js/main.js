9507cdd6ccd84c3955690fd497ddae69b538cf62
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2020-10-21 11:02:35 -0300

    adição da pasta js e arquivo main.js para aula sobre array e dicionarios

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var frutas = [
+  { nome: 'maçã', cor: 'vermelha' },
+  { nome: 'uva', cor: 'verde' },
+];
+console.log(frutas);
+console.log(frutas[0].nome);
+/* var lista = ['maça', 'pêra', 'banana'];
+lista.push('uva'); //inclui um item na lista
+lista.pop(); //exclui um item na lista
+console.log(lista);
+console.log(lista.toString()); //passa para string
+console.log(lista.join(' - ')); //adiciona uma máscara
+console.log(lista.reverse()); //imprime a lista em ordem inversa
+ */
